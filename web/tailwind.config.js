05a562ae3dfa30f9c271c4af7ddd32e71f892092
Richardson Bessa <richardsonlbessa@gmail.com>
2022-05-03 19:09:38 -0300

    add popover

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        richard: {
+          500: '#8257e6'
+        }
+      }
+    },
   },
   plugins: [],
 }
