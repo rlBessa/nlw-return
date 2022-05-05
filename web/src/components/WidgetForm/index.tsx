7318b7bf6b2d1c61de16a4a6ad5d8f044c40ccc5
Richardson Bessa <richardsonlbessa@gmail.com>
2022-05-04 23:17:00 -0300

    fininsh front end

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,15 @@
+/* Parei em 50:55 AULA 2!!!! */
+
 import { useState } from "react";
-import { CloseButtom } from "../CloseButtom";
+import { CloseButton } from "../CloseButton";
 import bugImageUrl from '../../images/bug.svg';
 import ideaImageUrl from '../../images/lightbulb.svg';
 import otherImageUrl from '../../images/chat-centered-text.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title:'Bug',
         image: {
@@ -28,38 +33,37 @@ const feedbackTypes = {
     },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+
+    function handleRestartFeedback(){
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto"> 
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>   
-                <CloseButtom />
-            </header> 
-
-            {!feedbackType ? (
-                <div className="flex py-8 gap-2 w-full">
-                    { Object.entries(feedbackTypes).map(([key, value]) => { 
-                        return(
-                            <button
-                                key={key}   
-                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col flex items-center gap-2 border-2 border-transparent hover:border-richard-500 focus:border-richard-500 focus:outline-none"
-                                onClick={() => setFeedbackType(key as FeedbackType)}
-                                type="button"
-
-                            >
-                                <img src={value.image.source} alt={value.image.alt}/>
-                                <span>{value.title}</span>
-                            </button>
-                        );
-                    }) }
-                </div>
+            { feedbackSent ? (
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-                <p>Ta funcionando!!!</p>
-            )}
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>    
+                    ) : (
+                        <FeedbackContentStep                     
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            )}    
+
+
 
             <footer className="text-xs text-neutral-400">
                 Powered by <a className="underline underline-offset-2" href="https://github.com/rlbessa">rlBessa</a>
